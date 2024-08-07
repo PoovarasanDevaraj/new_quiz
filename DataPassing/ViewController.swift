298f2290e47170ced606077440533c4d2db58a04
PoovarasanDevaraj <poovarasandev20.02@gmail.com>
2024-08-07 14:38:25 +0530

    Initial Commit

---
+++ DataPassing/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  DataPassing
+//
+//  Created by Poovarasan Devaraj on 07/08/24.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
---
+++ DataPassingTests/DataPassingTests.swift
@@ -0,0 +1,17 @@
+//
+//  DataPassingTests.swift
+//  DataPassingTests
+//
+//  Created by Poovarasan Devaraj on 07/08/24.
+//
+
+import Testing
+@testable import DataPassing
+
+struct DataPassingTests {
+
+    @Test func testExample() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
